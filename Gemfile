a6631920e8bdbe5b689c3d5aaa46f2186ef25a04
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-12-15 18:04:15 +0900

    devise

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "haml-rails", "~> 2.0"
 gem 'jquery-rails'
 gem 'font-awesome-sass'
+gem 'devise'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: "hirakei@yahoo.jp"
+  password: "mercari62A"
+  password_confirmation: "mercari62A"
+  admin: true
+)
