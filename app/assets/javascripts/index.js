139081b65c64627491ce3e6ee6081f3e04a3a986
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-12-15 11:40:52 +0900

    scroll

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,6 +3,18 @@ $(function(){
   $('.content-box').hide().fadeIn(1300);
   $('.content-box2').css("opacity", 0);
 
+  // 2列目のボックスを出現させる記述
+  $(window).scroll(function(){  
+    let imgPos = $('.content-box2').offset().top;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll > imgPos - windowHeight + windowHeight/5){
+      $('.content-box2').css("opacity", 0.5);
+    }
+    else {
+      $('.content-box2').css("opacity", 0);
+      console.log("erase");
+    }
 
-
+  })
 })
