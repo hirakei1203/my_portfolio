53f60c8fbb70448a66544eb0e3e0f18b6df9f17b
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-12-15 12:22:12 +0900

    header posi modify

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,6 @@
 $(function(){
 
   $('.content-box').hide().fadeIn(1300);
-  $('.content-box2').css("opacity", 0);
 
   // 2列目のボックスを出現させる記述
   $(window).scroll(function(){  
@@ -12,4 +11,7 @@ $(function(){
       $('.content-box2').css("opacity", 0.5);
     }
   })
+
+  
+
 })
