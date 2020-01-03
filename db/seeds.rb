17d8246e8a522c59de261c893e2f866818ba149a
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-03 23:06:30 +0900

    admin2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  email: "hirakei@gmail.com",
-  password: "mercari62A",
-  password_confirmation: "mercari62A",
-  admin: true
-)
+# User.create!(
+#   email: "hirakei@gmail.com",
+#   password: "mercari62A",
+#   password_confirmation: "mercari62A",
+#   admin: true
+# )
 
-Product.create!(
-  name: "Job Log",
-  image: "Job_Log_image.png",
-  address: "https://app-hk1.herokuapp.com/",
-  description: "ブログ風業務改善アプリです。初アプリです！",
-  status_id: 1,
-  language: "Ruby",
-  technique: "Ruby on rails, MySQL, heroku"
-)
+# Product.create!(
+#   name: "Job Log",
+#   image: "Job_Log_image.png",
+#   address: "https://app-hk1.herokuapp.com/",
+#   description: "ブログ風業務改善アプリです。初アプリです！",
+#   status_id: 1,
+#   language: "Ruby",
+#   technique: "Ruby on rails, MySQL, heroku"
+# )
 
-Product.create!(
-  name: "メルカリコピーサイト",
-  image: "page_mercari2.png",
-  address: "http://13.115.135.138/",
-  description: "メルカリのコピーサイトです。6人チームで作りました! (ID: 62_mercari_a, Pass: 62a)",
-  status_id: 1,
-  language: "Ruby",
-  technique: "Ruby on rails, MySQL, AWS"
-)
+# Product.create!(
+#   name: "メルカリコピーサイト",
+#   image: "page_mercari2.png",
+#   address: "http://13.115.135.138/",
+#   description: "メルカリのコピーサイトです。6人チームで作りました! (ID: 62_mercari_a, Pass: 62a)",
+#   status_id: 1,
+#   language: "Ruby",
+#   technique: "Ruby on rails, MySQL, AWS"
+# )
 
-Product.create!(
-  name: "Rintaro顔認識アプリ",
-  image: "Rin_app_image.png",
-  address: "https://tranquil-gorge-10782.herokuapp.com/",
-  description: "OpenCVを使用した機械学習による顔認識アプリです",
-  status_id: 1,
-  language: "Python",
-  technique: "Flask, Anaconda, heroku"
-)
+# Product.create!(
+#   name: "Rintaro顔認識アプリ",
+#   image: "Rin_app_image.png",
+#   address: "https://tranquil-gorge-10782.herokuapp.com/",
+#   description: "OpenCVを使用した機械学習による顔認識アプリです",
+#   status_id: 1,
+#   language: "Python",
+#   technique: "Flask, Anaconda, heroku"
+# )
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
